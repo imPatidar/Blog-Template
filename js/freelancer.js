cd49e71e3320708a0e717debab377b9b73410096
Pankaj M. Patidar <39170113+imPatidar@users.noreply.github.com>
2018-11-18 20:48:03 +0530

    Update freelancer.js

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,6 +1,6 @@
 (function($) {
   "use strict"; // Start of use strict
-// Scrolling Smooth
+// Scrolling Smooth Using 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
